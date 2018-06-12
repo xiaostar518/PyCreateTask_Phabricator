776d7c43d29f3da38038740c14b912aba19b7bed
xiao.xiao2 <xiao.xiao2@byd.com>
2018-06-12 09:25:59 +0800

    Updated load excel function.

--- src/py_excel/excel.py
+++ src/py_excel/excel.py
@@ -3,7 +3,7 @@
 
 from openpyxl import load_workbook
 from openpyxl import Workbook
-import json
+import os
 
 
 class OperateExcel:
@@ -16,37 +16,45 @@ class OperateExcel:
     def load_excel(self):
         print("loading Excel")
         # 打开一个workbook
-        wb = load_workbook(self.load_filename)
+        if not os.path.exists(self.load_filename):
+            print "No corresponding Excel file found."
+            exit(0)
 
-        # print(wb.sheetnames)
-        sheetnames = wb.sheetnames
-        first_sheet = wb[sheetnames[0]]
+        try:
+            wb = load_workbook(self.load_filename)
+        except Exception, e:
+            print "Excel is wrong : ", e
+            exit(0)
+        else:
+            # print(wb.sheetnames)
+            sheetnames = wb.sheetnames
+            first_sheet = wb[sheetnames[0]]
 
-        print "Work Sheet Title : ", first_sheet.title
-        # print "Work Sheet Max Rows:", first_sheet.max_row
-        # print "Work Sheet Min Rows:", first_sheet.min_row
-        # print "Work Sheet Max Cols:", first_sheet.max_column
-        # print "Work Sheet Min Cols:", first_sheet.min_column
+            print "Work Sheet Title : ", first_sheet.title
+            # print "Work Sheet Max Rows:", first_sheet.max_row
+            # print "Work Sheet Min Rows:", first_sheet.min_row
+            # print "Work Sheet Max Cols:", first_sheet.max_column
+            # print "Work Sheet Min Cols:", first_sheet.min_column
 
-        post_data = []
-        headers = []
-        for row in range(first_sheet.min_row, first_sheet.max_row + 1):
-            post_content = {}
-            for column in range(first_sheet.min_column, first_sheet.max_column + 1):
-                if row == 1:
-                    value = first_sheet.cell(row=row, column=column).value
-                    headers.append(value.lower().replace(' ', '').replace('\n', ''))
-                else:
-                    if column == 1 and not first_sheet.cell(row=row, column=column).value:
-                        break
+            post_data = []
+            headers = []
+            for row in range(first_sheet.min_row, first_sheet.max_row + 1):
+                post_content = {}
+                for column in range(first_sheet.min_column, first_sheet.max_column + 1):
+                    if row == 1:
+                        value = first_sheet.cell(row=row, column=column).value
+                        headers.append(value.lower().replace(' ', '').replace('\n', ''))
                     else:
-                        post_content[headers[column - 1]] = first_sheet.cell(row=row, column=column).value
+                        if column == 1 and not first_sheet.cell(row=row, column=column).value:
+                            break
+                        else:
+                            post_content[headers[column - 1]] = first_sheet.cell(row=row, column=column).value
 
-            if post_content and post_content:
-                post_data.append(post_content)
+                if post_content and post_content:
+                    post_data.append(post_content)
 
-        # print "excel : ", post_data
-        return post_data
+            # print "excel : ", post_data
+            return post_data
 
     # 将数据导出到excel文件中
     def export_excel(self, datas=None):
